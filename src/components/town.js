e7ded3b1d633f9e0d32a46e9993583cfeda8faa2
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2021-02-05 22:46:22 +0000

    feat(weather): adds weather icon

--- src/components/town.js
+++ src/components/town.js
@@ -15,6 +15,12 @@ export default function Town() {
           <text y="35">sunset: {state.sunset} </text>
           <text y="55">icon: {state.icon} </text>
           <text y="75">icon_phrase: {state.icon_phrase} </text>
+          <image
+            href={ `https://developer.accuweather.com/sites/default/files/${ state.icon.padStart(2, '0') }-s.png` }
+            x="0"
+            y="75"
+            height="75px"
+            width="45px"/>
         </g>
     </svg>
   )
