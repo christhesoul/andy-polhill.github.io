fc4528674f8068b9e49831361cd1b70ae68fdc10
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2021-02-05 21:33:12 +0000

    feat(gatsby): use global context

--- .github/actions/write-property/index.js
+++ .github/actions/write-property/index.js
@@ -1,30 +1,18 @@
 const core = require('@actions/core');
 const fs = require('fs');
-const simple_git = require('simple-git');
 
-const git = simple_git('');
+const config_file = './config.json';
 
 try {
   const key = core.getInput('key');
   const value = core.getInput('value');
-  const token = core.getInput('token');
-  const config = JSON.parse(fs.readFileSync('./config.json'));
-  fs.writeFileSync('./config.json', JSON.stringify({
+  const config = JSON.parse(fs.readFileSync(config_file));
+
+  fs.writeFileSync(config_file, JSON.stringify({
     ...config,
     [key]: `${value}`
   }, null, 2))
-
-  return git
-    .init()
-    .addConfig('user.name', 'virtual-town-mayor')
-    .addConfig('user.email', '78474328+virtual-town-mayor@users.noreply.github.com')
-    .remote([ 'set-url', 'origin', `https://virtual-town-mayor:${token}@github.com/${process.env.GITHUB_REPOSITORY}`])
-    .fetch()
-    .add('./config.json')
-    .commit(`feat:(${key}): update value ${value}`)
-    .push('origin', 'HEAD:gh-pages', ['-u', '-f'])
-    .catch((error) => core.setFailed(error.message));
-
+  
 } catch (error) {
   core.setFailed(error.message);
 }
