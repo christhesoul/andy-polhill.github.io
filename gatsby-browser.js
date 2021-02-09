2a3a8006daaf2e037b7e5214e784eb1eb7deed54
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2021-02-09 22:49:17 +0000

    feat(night): render darker background at night

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from "react"
 import GlobalContextProvider from "./src/context/GlobalContextProvider"
 
+import "./src/styles/global.css"
+
 export const wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>
 }
--- src/components/town.js
+++ src/components/town.js
@@ -2,14 +2,36 @@ import React, { useContext } from "react"
 import {
   GlobalStateContext,
 } from "../context/GlobalContextProvider"
+import classNames from 'classnames';
+
+import styles from "./town.module.css"
 
 export default function Town() {
   const state = useContext(GlobalStateContext);
 
+  const now = new Date();
+  const sunrise = new Date(Date.parse(state.sunrise));
+  const sunset = new Date(Date.parse(state.sunset));
+  const ms_since_midnight = new Date(now) - now.setHours(0,0,0,0);
+
+  let night = false;
+  if(
+      ms_since_midnight > new Date(sunrise) - sunrise.setHours(0,0,0,0) ||
+      ms_since_midnight < new Date(sunset) - sunset.setHours(0,0,0,0)
+    ) {
+      night = true;
+  }
+
+  const classes = classNames({
+    [styles.town]: true,
+    [styles.town__night]: night,
+  });
+
   return (
     <svg version="1.1"
-        width="300" height="200"
-        xmlns="http://www.w3.org/2000/svg">
+        width="100%" height="300"
+        xmlns="http://www.w3.org/2000/svg"
+        className={classes}>
         <g>
           <text y="15">sunrise: {state.sunrise} </text>
           <text y="35">sunset: {state.sunset} </text>
