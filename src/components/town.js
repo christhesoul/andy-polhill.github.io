f9e1ef4a2d7f59449198dabf28ab0891edbbe76f
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2021-02-16 22:34:25 +0000

    feat(timeOfDay): adds basic sunrise/sunset effect

--- src/components/town.js
+++ src/components/town.js
@@ -6,32 +6,52 @@ import classNames from 'classnames';
 
 import styles from "./town.module.css"
 
+const half_hour = 3600000 / 2;
+
 export default function Town() {
 
   const state = useContext(GlobalStateContext);
   const now = new Date();
   const sunrise = new Date(Date.parse(state.sunrise));
   const sunset = new Date(Date.parse(state.sunset));
-  const ms_since_midnight = new Date(now) - now.setHours(0,0,0,0);
-  const after_sunrise = ms_since_midnight > new Date(sunrise) - sunrise.setHours(0,0,0,0);
-  const before_sunset = ms_since_midnight < new Date(sunset) - sunset.setHours(0,0,0,0);
+  const time = new Date(now) - now.setHours(0,0,0,0);
+  const ms_to_sunrise = new Date(sunrise) - sunrise.setHours(0,0,0,0);
+  const ms_to_sunset = new Date(sunset) - sunset.setHours(0,0,0,0);
 
-  const [daytime, setDaytime] = useState(true, after_sunrise,  before_sunset);
+  const [timeOfDay, setTimeOfDay] = useState('day', ms_to_sunrise,  ms_to_sunset, time);
 
-  useEffect(() => setDaytime(after_sunrise && before_sunset), [after_sunrise, before_sunset]);
+  useEffect(() => {
+    if((time > ms_to_sunrise - half_hour) &&
+      (time < ms_to_sunrise + half_hour)) {
+      return setTimeOfDay('sunrise');
+    }
+
+    if((time > ms_to_sunset - half_hour) &&
+      (time < ms_to_sunset + half_hour)) {
+      return setTimeOfDay('sunset');
+    }
+
+    if((time > ms_to_sunset + half_hour) ||
+      (time < ms_to_sunrise - half_hour)) {
+      return setTimeOfDay('night');
+    }
+
+    return setTimeOfDay('day');
+  }, [ms_to_sunrise, ms_to_sunset, time]);
 
   const classes = classNames({
     [styles.town]: true,
-    [styles.town__night]: !daytime,
+    [styles.town__night]: timeOfDay === 'night',
+    [styles.town__sunrise]: timeOfDay === 'sunrise',
+    [styles.town__sunset]: timeOfDay === 'sunset',
   });
 
-  console.log(state);
-
   return (
-    <svg version="1.1"
-        width="100%" height="300"
-        xmlns="http://www.w3.org/2000/svg"
-        className={classes}>
-    </svg>
+    <div className={classes}>
+      <svg version="1.1"
+          width="100%" height="300"
+          xmlns="http://www.w3.org/2000/svg">
+      </svg>
+    </div>
   )
 }
