bbbd25a8ad8c14fde592be6c7e74b305d671f46d
Andrew Polhill <2196085+andy-polhill@users.noreply.github.com>
2021-03-26 22:12:16 +0000

    refactor(sky): use group instead of Fragment

--- src/components/sky/sky.js
+++ src/components/sky/sky.js
@@ -4,15 +4,15 @@ import PropTypes from "prop-types";
 export default function Sky({ timeOfDay }) {
 
   return (
-    <Fragment>
+    <g id="sky">
       <defs>
-        <linearGradient id="sky" gradientTransform="rotate(90)">
+        <linearGradient id="sky-gradient" gradientTransform="rotate(90)">
           <stop offset="5%"  stopColor={ `var(--sky-top-${timeOfDay})` } />
           <stop offset="95%" stopColor={ `var(--sky-bottom-${timeOfDay})` } />
         </linearGradient>
       </defs>
-      <rect fill="url(#sky)" height="100%" width="100%"/> 
-    </Fragment>
+      <rect fill="url(#sky-gradient)" height="100%" width="100%"/> 
+    </g>
   )
 }
 
