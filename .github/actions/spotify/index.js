9547ab9729a082bff82a857760aad64beb02e9d8
Andy Polhill <2196085+andy-polhill@users.noreply.github.com>
2021-09-23 22:15:16 +0100

    feat(actions): adds spotify top tracks

---
+++ .github/actions/spotify/index.js
@@ -0,0 +1,72 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+const getTopTracks = async () => {
+  const time_range = core.getInput('time_range');
+  const client_id = core.getInput('client-id');
+  const client_secret = core.getInput('client-secret');
+
+  console.log('get tracks');
+
+  const encodedToken = Buffer
+    .from(`${client_id}:${client_secret}`)
+    .toString("base64");
+
+  console.log('encodedToken: ', encodedToken.length);
+
+  const { data } = await axios.post("https://accounts.spotify.com/api/token", 
+    stringify({ grant_type: "client_credentials" }), { 
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encodedToken}`
+      }
+    });
+
+  console.log('access_token: ', data.access_token.length);
+
+  axios.defaults.headers.common["Authorization"] = `Bearer ${data.access_token}`;
+  axios.defaults.headers.common["Accept"] = "application/json";
+  axios.defaults.headers.common["Content-Type"] = "application/json";
+
+  const { data } = await axios.get(`https://api.spotify.com/v1/me/top/tracks`, {
+    params: {
+      time_range,
+      limit: 1
+    }
+  })
+
+  console.log('output')
+  console.log(data);
+
+  return data;
+}
+
+try {
+  const data = getTopTracks();
+
+  console.dir(data.items[0]);
+
+  core.setOutput("track", "test-tracke");
+  core.setOutput("artist", "test-artist");
+  core.setOutput("href", "test-url");
+
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
+
+
+
+// console.log(response.data.DailyForecasts[0]);
+
+// core.setOutput("sunrise", response.data.DailyForecasts[0].Sun.Rise);
+// core.setOutput("sunset", response.data.DailyForecasts[0].Sun.Set);
+// core.setOutput("icon", response.data.DailyForecasts[0].Day.Icon);
+// core.setOutput("iconPhrase", response.data.DailyForecasts[0].Day.IconPhrase);
+// core.setOutput("cloudCover", response.data.DailyForecasts[0].Day.CloudCover);
+// core.setOutput("moonRise", response.data.DailyForecasts[0].Moon.Rise);
+// core.setOutput("moonSet", response.data.DailyForecasts[0].Moon.Set);
+// core.setOutput("moonPhase", response.data.DailyForecasts[0].Moon.Phase);
+// })
+// .catch((error) => core.setFailed(error))
