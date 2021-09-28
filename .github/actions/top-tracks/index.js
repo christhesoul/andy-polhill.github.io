c3e37f89813011849ae3d1f3813ff3149829278b
Andy Polhill <2196085+andy-polhill@users.noreply.github.com>
2021-09-28 08:59:18 +0100

    work journey

--- .github/actions/top-tracks/index.js
+++ .github/actions/top-tracks/index.js
@@ -1,23 +1,28 @@
-const core = require('@actions/core');
+// const core = require('@actions/core');
 const axios = require('axios');
 const querystring = require('querystring');
 
 const getTopTracks = async () => {
-  const time_range = core.getInput('time_range');
-  const client_id = core.getInput('client_id');
-  const client_secret = core.getInput('client_secret');
+  // const time_range = core.getInput('time_range');
+  // const client_id = core.getInput('client_id');
+  // const client_secret = core.getInput('client_secret');
+
+  const time_range = 'short_term';
+  const client_id = 'f2ce966eef3f46318a59e648dfb08754'
+  const client_secret = '597bdb6c6a724514a1a321146aa450ac';
 
   console.log('client_id: ', client_id);
   console.log('client_secret: ', client_secret);
   console.log('get tracks');
 
+  debugger;
+
   const encodedToken = Buffer
     .from(`${client_id}:${client_secret}`)
     .toString("base64");
 
-  console.log('encodedToken: ', encodedToken.length);
-
   try {
+
     const { data } = await axios.post("https://accounts.spotify.com/api/token", 
       querystring.stringify({ grant_type: "client_credentials" }), { 
         headers: {
@@ -28,18 +33,25 @@ const getTopTracks = async () => {
 
     console.log('access_token: ', data.access_token);
 
-    axios.defaults.headers.common["Authorization"] = `Bearer ${data.access_token}`;
-    axios.defaults.headers.common["Accept"] = "application/json";
-    axios.defaults.headers.common["Content-Type"] = "application/json";
-
-
+      data.access_token = "BQDgi_kxoxCHBLumk0W4irWwhxyHyp3ygkFi0nVlnfO_olfbm7jYAyEH9wccUcPrjhMTcK5LU1tgDV3lnVt-CpQfNJrkNl1njfFqueGvznaDrKFbpukYbTopyfBL5aYk5lwlj8baKn0J0h2UjUysLsTdj4X9fek";
 
     const { data: track_data } = await axios.get(`https://api.spotify.com/v1/me/`, { // top/tracks
       params: {
         time_range,
         limit: 1
+      },
+      headers: {
+        "Authorization": `Bearer ${data.access_token}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json"
       }
-    })
+    }, {
+      headers: {
+        "Authorization": `Bearer ${data.access_token}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    });
 
     console.log('output')
     console.log(track_data);
@@ -55,12 +67,12 @@ try {
 
   console.dir(data.items[0]);
 
-  core.setOutput("track", "test-tracke");
-  core.setOutput("artist", "test-artist");
-  core.setOutput("href", "test-url");
+  // core.setOutput("track", "test-tracke");
+  // core.setOutput("artist", "test-artist");
+  // core.setOutput("href", "test-url");
 
 } catch (error) {
-  core.setFailed(error.message);
+  // core.setFailed(error.message);
 }
 
 
