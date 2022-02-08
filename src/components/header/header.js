c54af0388a116a9b748a560bdff43fce7baf8d50
Andy Polhill <2196085+andy-polhill@users.noreply.github.com>
2022-02-08 22:34:37 +0000

    feat(social): adds social links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
 import { Link } from "gatsby";
 import React from "react";
+import LinkedInIcon from "../icon/linkedin";
+import TwitterIcon from "../icon/twitter";
 import * as styles from "./header.module.css";
 
 export default function Header({}) {
@@ -7,15 +9,23 @@ export default function Header({}) {
     <header className={ styles.header }>
       <nav className={ styles.nav }>
         <ul>
-          <li>
+          <li className={ styles.navItem }>
             <Link to="/">
               Home
             </Link>
           </li>
-          <li>
+          <li className={ styles.navItem }>
             <Link to="/blog">
               Blog
             </Link>
+          </li>
+          <li className={` ${styles.navItem} ${styles.socialIcons}` }>
+            <a href="twitter.com/andy-polhill" target="_blank">
+              <TwitterIcon />
+            </a>
+            <a href="linkedin.com/in/andy-polhill" target="_blank">
+              <LinkedInIcon />
+            </a>
           </li>
         </ul>
       </nav>
